4f179af96e8c5ea15584295021823dbd997e0007
Ro0t-set <ro0trexustom@gmail.com>
2020-11-22 13:35:04 +0100

    es 4

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,5 +1,19 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
 /**
  * A very simple program using a graphical interface.
  * 
@@ -10,26 +24,87 @@ public final class SimpleGUIWithFileChooser {
      * TODO: Starting from the application in mvcio:
      * 
      * 1) Add a JTextField and a button "Browse..." on the upper part of the
-     * graphical interface.
-     * Suggestion: use a second JPanel with a second BorderLayout, put the panel
-     * in the North of the main panel, put the text field in the center of the
-     * new panel and put the button in the line_end of the new panel.
+     * graphical interface. Suggestion: use a second JPanel with a second
+     * BorderLayout, put the panel in the North of the main panel, put the text
+     * field in the center of the new panel and put the button in the line_end of
+     * the new panel.
      * 
-     * 2) The JTextField should be non modifiable. And, should display the
-     * current selected file.
+     * 2) The JTextField should be non modifiable. And, should display the current
+     * selected file.
      * 
-     * 3) On press, the button should open a JFileChooser. The program should
-     * use the method showSaveDialog() to display the file chooser, and if the
-     * result is equal to JFileChooser.APPROVE_OPTION the program should set as
-     * new file in the Controller the file chosen. If CANCEL_OPTION is returned,
-     * then the program should do nothing. Otherwise, a message dialog should be
-     * shown telling the user that an error has occurred (use
+     * 3) On press, the button should open a JFileChooser. The program should use
+     * the method showSaveDialog() to display the file chooser, and if the result is
+     * equal to JFileChooser.APPROVE_OPTION the program should set as new file in
+     * the Controller the file chosen. If CANCEL_OPTION is returned, then the
+     * program should do nothing. Otherwise, a message dialog should be shown
+     * telling the user that an error has occurred (use
      * JOptionPane.showMessageDialog()).
      * 
      * 4) When in the controller a new File is set, also the graphical interface
-     * must reflect such change. Suggestion: do not force the controller to
-     * update the UI: in this example the UI knows when should be updated, so
-     * try to keep things separated.
+     * must reflect such change. Suggestion: do not force the controller to update
+     * the UI: in this example the UI knows when should be updated, so try to keep
+     * things separated.
      */
+    private static final String TITLE = "GUI application";
+
+    private final JFrame frame = new JFrame(TITLE);
+
+    public SimpleGUIWithFileChooser() {
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+
+        final Controller controller = new Controller();
+
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+
+        final JButton save = new JButton("save");
+        canvas.add(save, BorderLayout.SOUTH);
+
+        final JPanel canvas2 = new JPanel();
+        canvas.add(canvas2, BorderLayout.NORTH);
+
+        final JTextArea text = new JTextArea(controller.getFile().toString());
+        text.setEditable(false);
+        canvas2.add(text, BorderLayout.CENTER);
+
+        final JButton browse = new JButton("Broswer");
+        canvas2.add(browse, BorderLayout.EAST);
+
+        browse.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+
+                final JFileChooser fc = new JFileChooser("Choose where to save");
+                fc.showSaveDialog(frame);
+                text.setText(fc.getSelectedFile().getPath());
+            }
+        });
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+
+                controller.nextString(text.getText());
+            }
+        });
+
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    /**
+     * @param args
+     *                 ignored
+     */
+    public static void main(final String... args) {
+        new SimpleGUIWithFileChooser();
+    }
 
 }
